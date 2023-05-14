bdcb8dda676dc168b04c6da931ffb2e8b1158225
fenixchen <chenchmail@gmail.com>
2023-05-15 07:21:58 +0800

    Add

---
+++ flask/Flask-05-get-post/get/run.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    username = request.args.get('username')
+    password = request.args.get('password')
+    if username is None or password is None:
+        return render_template('index.html')
+    
+    if username == "xugaoxiang" and password == "xugaoxiang":
+        return f"<html><body>Welcome {username}</body></html>"
+    else:
+        return f"<html><body>Welcome!</body></html>"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
